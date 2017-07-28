9d12edda64b030c97789b4c2059e4d8b24e16221
Tiago Ribeiro de Souza <tiago.astro@gmail.com>
2017-07-28 11:30:00 -0300

    Improvements and bug fixes.

--- chimera_qc/controllers/qualitycontrol.py
+++ chimera_qc/controllers/qualitycontrol.py
@@ -1,8 +1,10 @@
+import time
 import datetime
 import json
 import os
 import threading
 from tempfile import mktemp
+import ntpath
 
 import numpy as np
 import requests
@@ -10,92 +12,11 @@ from chimera.core.callback import callback
 from chimera.core.chimeraobject import ChimeraObject
 from chimera.core.manager import Manager
 from chimera.interfaces.camera import CameraStatus
-from chimera.util.image import Image
+from chimera.util.image import ImageUtil, Image
+from chimera.controllers.imageserver.util import getImageServer
+from chimera.core.exceptions import ChimeraException
 
 from chimera_qc.controllers.model import Session, ImageStatistics
-
-
-# class SchedCallbacks(object):
-#     def __init__(self, localManager, schedname, data):
-#         self._data = data
-#         self.schedname = schedname
-#
-#         @callback(localManager)
-#         def SchedActionBeginClbk(action, message):
-#             self.update_sched_msg(message)
-#
-#         @callback(localManager)
-#         def SchedStateChangedClbk(newState, oldState):
-#             self.update_sched_state(newState)
-#
-#         self.SchedStateChangedClbk = SchedStateChangedClbk
-#         self.SchedActionBeginClbk = SchedActionBeginClbk
-#
-#     def update_sched_state(self, state):
-#         self._data.update({'state': str(state),
-#                            'last_update': datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')})
-#         print self._data
-#
-#     def update_sched_msg(self, message):
-#         self._data['message'] = message
-#         self._data['last_update'] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-#         print self._data
-
-
-class CameraCallbacks(object):
-    def __init__(self, localManager, sex_params, filters):
-        self.sex_params = sex_params
-        # self.stats = []
-        self._threadList = []
-
-        @callback(localManager)
-        def CamerareadoutComplete(proxy, status):
-            p = threading.Thread(target=self.run_stats, args=(proxy, status))
-            # self._threadList.append(p)
-            p.start()
-
-        self.CamerareadoutCompleteClbk = CamerareadoutComplete
-
-    def run_stats(self, proxy, status):
-        if status == CameraStatus.OK and proxy["IMAGETYP"].upper().rstrip() == "OBJECT" and \
-                        proxy["SHUTTER"].upper().rstrip() == "OPEN":
-
-            print proxy.filename(), proxy.keys(), status, proxy.http(), status
-
-            # TODO: Make this work locally too.
-            tmpfile = mktemp()
-            r = requests.get(proxy.http(), stream=True)
-            with open(tmpfile, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024):
-                    if chunk:  # filter out keep-alive new chunks
-                        f.write(chunk)
-            img = Image.fromFile(tmpfile)
-            p = self.sex_params
-            p.update({"CATALOG_NAME": mktemp()})
-            extract = img.extract(p)
-            os.unlink(tmpfile)
-            # else:
-            # extract = proxy.extract(self.sex_params)
-
-            if len(extract) > 0:  # Only go ahead if at least one object was detected
-                stats = np.array(
-                    [[data["CLASS_STAR"], data["FLAGS"], data["FWHM_IMAGE"], data["BACKGROUND"]] for data in
-                     extract])
-                mask = np.bitwise_and(stats[:, 0] > 0.8, stats[:, 1] == 0)
-                fff = proxy["FILTER"]
-                # fff = "R"
-                session = Session()
-                try:
-                    log = ImageStatistics(
-                        date_obs=datetime.datetime.strptime(proxy["DATE-OBS"], "%Y-%m-%dT%H:%M:%S.%f"),
-                        filename=proxy.filename(), filter=fff, fwhm_avg=np.average(stats[:, 2][mask]),
-                        fwhm_std=np.std(stats[:, 2][mask]), background=np.average(stats[:, 3][mask]), npts=mask.sum(),
-                        exptime=proxy["EXPTIME"])
-                    session.add(log)
-                finally:
-                    session.commit()
-                    # self.stats.append(s)
-                    # print "fwhm stats:", s  # self.stats[-1]
 
 
 class QualityControl(ChimeraObject):
@@ -108,7 +29,7 @@ class QualityControl(ChimeraObject):
     def _getCam(self):
         return self.getManager().getProxy(self["camera"])
 
-    def _getSched(self):
+    def getSched(self):
         return self.getManager().getProxy(self["scheduler"])
 
     def __init__(self):
@@ -129,18 +50,14 @@ class QualityControl(ChimeraObject):
         self._sex_params['PARAMETERS_LIST'] = ["NUMBER", "XWIN_IMAGE", "YWIN_IMAGE", "FLUX_BEST", "FWHM_IMAGE", "FLAGS",
                                                "CLASS_STAR", "BACKGROUND"]
 
-        # FIXME: This should not be hardcoded!
-        self.localManager = Manager("192.168.50.107", 9001)
-        # self.localManager = Manager("127.0.0.1", 9001)
-
         # self._data = dict()
         # self.sched_callbacks = SchedCallbacks(self.localManager, self["scheduler"].split('/')[-1], self._data)
         #
         # self.getManager().getProxy(self["scheduler"]).actionBegin += self.sched_callbacks.SchedActionBeginClbk
         # self.getManager().getProxy(self["scheduler"]).stateChanged += self.sched_callbacks.SchedStateChangedClbk
         self.filters = self._getCam().getFilters()
-        self.camera_callbacks = CameraCallbacks(self.localManager, self._sex_params, self.filters)
-        self._getCam().readoutComplete += self.camera_callbacks.CamerareadoutCompleteClbk
+        # self.camera_callbacks = CameraCallbacks(self.getManager(), self._sex_params, self.filters)
+        self._getCam().readoutComplete += self.getProxy()._CameraReadoutCompleteClbk
         self.stats = {f: {} for f in self.filters}
 
     def control(self):
@@ -181,6 +98,66 @@ class QualityControl(ChimeraObject):
                                   background=[e.background for e in stats])
         session.commit()
         return ret
+
+    def run_stats(self, proxy, status):
+        if status == CameraStatus.OK and proxy["IMAGETYP"].upper().rstrip() == "OBJECT" and \
+                        proxy["SHUTTER"].upper().rstrip() == "OPEN":
+
+            self.log.debug('%s [%s][%s]: @[%s]' % (proxy.filename(), proxy.keys(), status, proxy.http()))
+
+            image_path = proxy.filename()
+            if not os.path.exists(image_path):  # If image is on a remote server, donwload it.
+
+                #  If remote is windows, image_path will be c:\...\image.fits, so use ntpath instead of os.path.
+                if ':\\' in image_path:
+                    modpath = ntpath
+                else:
+                    modpath = os.path
+                image_path = ImageUtil.makeFilename(os.path.join(getImageServer(self.getManager()).defaultNightDir(),
+                                                                 modpath.basename(image_path)))
+                t0 = time.time()
+                self.log.debug('Downloading image from server to %s' % image_path)
+                if not ImageUtil.download(proxy, image_path):
+                    raise ChimeraException('Error downloading image %s from %s' % (image_path, image.http()))
+                self.log.debug('Finished download. Took %3.2f seconds' % (time.time() - t0))
+                img = Image.fromFile(image_path)
+            else:
+                img = Image.fromFile(image_path)
+
+            tmpfile = mktemp()
+            p = self._sex_params
+            p.update({"CATALOG_NAME": mktemp()})
+            extract = img.extract(p)
+            # os.unlink(tmpfile)
+            # else:
+            # extract = proxy.extract(self.sex_params)
+
+            if len(extract) > 0:  # Only go ahead if at least one object was detected
+                stats = np.array(
+                    [[data["CLASS_STAR"], data["FLAGS"], data["FWHM_IMAGE"], data["BACKGROUND"]] for data in
+                     extract])
+                mask = np.bitwise_and(stats[:, 0] > 0.8, stats[:, 1] == 0)
+                fff = "CLEAR"
+                if "FILTER" in proxy.keys():
+                    fff = proxy["FILTER"]
+                # fff = "R"
+                session = Session()
+                try:
+                    log = ImageStatistics(
+                        date_obs=datetime.datetime.strptime(proxy["DATE-OBS"], "%Y-%m-%dT%H:%M:%S.%f"),
+                        filename=proxy.filename(), filter=fff, fwhm_avg=np.average(stats[:, 2][mask]),
+                        fwhm_std=np.std(stats[:, 2][mask]), background=np.average(stats[:, 3][mask]), npts=mask.sum(),
+                        exptime=proxy["EXPTIME"])
+                    session.add(log)
+                finally:
+                    session.commit()
+                    # self.stats.append(s)
+                    # print "fwhm stats:", s  # self.stats[-1]
+
+    def _CameraReadoutCompleteClbk(self, proxy, status):
+        p = threading.Thread(target=self.run_stats, args=(proxy, status))
+        # self._threadList.append(p)
+        p.start()
 
     def __stop__(self):
         pass
